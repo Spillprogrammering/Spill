9e2370d4b5955608f3d977cfdef30cecd78e3704
Mwinnem <marcuswinnem5@hotmail.com>
2014-05-29 18:44:15 +0200

    test

---
+++ Game/Forms/HiScore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game.Forms
+{
+    public partial class HiScore : Form
+    {
+        public HiScore()
+        {
+            InitializeComponent();
+        }
+
+        //Metode som kjøres når HiScore formen åpnes
+        private void HiScore_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hiScoreDataSet.Hiscore' table. You can move, or remove it, as needed.
+            this.hiscoreTableAdapter.FillByScore(this.hiScoreDataSet.Hiscore);
+
+        }
+
+    }
+}
--- Game/Properties/Settings.Designer.cs
+++ Game/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Game.Properties
-{
-
-
+namespace Game.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=kark.hin.no;user id=bjornrp;password=bjornrp123;persistsecurityinfo=True;d" +
+            "atabase=BRP_DB1")]
+        public string BRP_DB1ConnectionString {
+            get {
+                return ((string)(this["BRP_DB1ConnectionString"]));
             }
         }
     }
